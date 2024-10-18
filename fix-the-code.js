1150ecb40b8c4004aab1201de6f11325cad87dc2
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-10-18 15:09:02 +0000

    Create fix-the-code.js

---
+++ fix-the-code.js
@@ -0,0 +1,5 @@
+/**
+We wrote a function, rollTheDice(), which is supposed to simulate two 
+dice being rolled and totalled. It’s close to doing what we want, but there’s something not quite right. 
+Can you fix our code, please?
+**/

358ec5879a65406ca42773bb5f30bc5d6ec6aca2
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-10-18 15:21:53 +0000

    Create trythOrFalsy.js

---
+++ trythOrFalsy.js
@@ -0,0 +1,12 @@
+/**
+
+It can be hard to keep track of what’s truthy or falsy in JavaScript. Write a function, truthyOrFalsy(), 
+that takes in any value and returns true if that value is truthy and false if that value is falsy.
+
+*/
+
+
+const truthyOrFalsy = (value) =>  value ? true : false;
+
+
+console.log(truthyOrFalsy(0)) // Should print false
